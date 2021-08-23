479ebeda181df5cd334b854cee164212e081ac4a
Christian Bermeo <czaudiovisual@hotmail.com>
2021-08-23 13:48:29 -0400

    commit tree 589d0997bd40c4eebd11cb522ed68c8b8e1bd810

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(int)
+
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
+
 end
+
+countdown(5)
+
+
+def countdown_with_sleep(int)
+    while int >= 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+end
+"HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(5)
